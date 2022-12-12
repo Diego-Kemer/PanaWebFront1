1464fa2e46d08530dc67a9f7dde084b6fcfc1bb2
Diego-Kemer <diegokemer26@gmail.com>
2022-12-12 13:12:26 -0300

    subiendo algo

--- src/app/module-home/components/principal/header/header.component.ts
+++ src/app/module-home/components/principal/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @ViewChild('quedarse') quedarse!: ElementRef;
+  @ViewChild('irse') irse!: ElementRef;
+  public imagen: string = 'assets/img/img4.jpg';
+  public imagenAnimada: string = 'assets/img/img4.jpg'
+  public imagenes: Array<string> = ['assets/img/img1.jpg', 'assets/img/img2.jpg', 'assets/img/img3.jpg', 'assets/img/img4.jpg'];
+  public numero: number = 0;
 
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
+    // this.imagen = this.imagenes[3]
+    interval(5000).subscribe(v =>{
+      v > 0 ? this.imagenAnimada = this.imagen : false;
+      this.numero < this.imagenes.length ? this.imagen = this.imagenes[this.numero] : false;
+      this.numero++;
+      this.numero >= this.imagenes.length ? this.numero = 0 : false;
+      this.renderer.addClass(this.quedarse.nativeElement, 'carousel__item--quedarse')
+      this.renderer.addClass(this.irse.nativeElement, 'carousel__item--irse')
+      setTimeout(()=>{
+        this.renderer.removeClass(this.quedarse.nativeElement, 'carousel__item--quedarse')
+        this.renderer.removeClass(this.irse.nativeElement, 'carousel__item--irse')
+      }, 1100)
+    })
+  }
+
+  nex(){
+    if(this.quedarse.nativeElement.classList[1] == 'carousel__item--quedarse'){return}
+    if(this.numero < this.imagenes.length){
+      this.imagenAnimada = this.imagen;
+      this.numero < this.imagenes.length ? this.imagen = this.imagenes[this.numero] : false;
+      this.numero++;
+      this.numero >= this.imagenes.length ? this.numero = 0 : false;
+      this.renderer.addClass(this.quedarse.nativeElement, 'carousel__item--quedarse')
+      this.renderer.addClass(this.irse.nativeElement, 'carousel__item--irse')
+      setTimeout(()=>{
+        this.renderer.removeClass(this.quedarse.nativeElement, 'carousel__item--quedarse')
+        this.renderer.removeClass(this.irse.nativeElement, 'carousel__item--irse')
+      }, 1000)
+    }else{console.log('mal')}
+  }
+
+  back(){
+    if(this.numero > 0){
+      if(this.imagen == this.imagenes[this.numero-1] && this.numero > 1){this.numero = this.numero - 2}else{this.numero--;} 
+      this.imagen = this.imagenes[this.numero]
+      console.log(this.numero)
+    }else{console.log('mal')}
   }
 
 }
--- src/app/module-shared/components/nav/nav.component.ts
+++ src/app/module-shared/components/nav/nav.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-nav',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  @ViewChild('enlaces') enlaces!: ElementRef;
+  @ViewChild('btn') btn!: ElementRef;
+  public icono: string = 'icofont-navigation-menu'
 
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
   }
-
+  hamburguesa(){
+    const elemento = this.enlaces.nativeElement;
+    if(elemento.classList[1] === 'hamburguesa__enlaces--entrada'){
+      this.renderer.removeClass(elemento, 'hamburguesa__enlaces--entrada')
+      this.renderer.addClass(elemento, 'hamburguesa__enlaces--salida')
+      this.icono = 'icofont-navigation-menu'
+      timer(950).subscribe(()=>{
+        this.renderer.removeClass(elemento, 'hamburguesa__enlaces--salida')
+      })
+    }else{
+      this.renderer.removeClass(elemento, 'hamburguesa__enlaces--salida')
+      this.renderer.addClass(elemento, 'hamburguesa__enlaces--entrada')
+      this.icono = 'icofont-close'
+    }
+    
+  }
 }
