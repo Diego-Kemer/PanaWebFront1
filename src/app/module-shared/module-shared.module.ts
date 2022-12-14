669c733eef6b63bf0684e79aea56e0e89d651d70
Diego-Kemer <diegokemer26@gmail.com>
2022-12-14 09:38:22 -0300

    acomodar responsive y funcionalidad en home

--- src/app/module-shared/module-shared.module.ts
+++ src/app/module-shared/module-shared.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { FormContactoComponent } from './components/form-contacto/form-contacto.component';
 
 
 
 @NgModule({
   declarations: [
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    FormContactoComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    FormContactoComponent
   ]
 })
 export class ModuleSharedModule { }
